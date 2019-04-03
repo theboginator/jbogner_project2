92bce1fefe2ad7d70eae7a6b065e5e8ab7b9403a
jbogner <boginator55@gmail.com>
2019-04-03 19:47:02 -0400

    Finish project requirements

--- src/PingClient.java
+++ src/PingClient.java
@@ -1,18 +1,61 @@
+/*
+PingClient application v1.0
+(c) 2019 by Jacob Bogner
+This application will send a series of pings over a specified port number and print out the response, if any,
+from any PingServer application running on the same port.
+ */
 import java.net.*;
 import java.util.*;
 
 public class PingClient {
     public static void main(String[] args) throws Exception{
-        int port = 2014;
-        DatagramSocket uplink = new DatagramSocket(port);
-        byte[] transferData = new byte[1024];
+        int port = 2014; //Declare a port
+        DatagramSocket uplink = new DatagramSocket();
+        InetAddress IPAddress = InetAddress.getByName("localhost");
+        byte[] transferData;
+        byte[] receiveData = new byte[1024];
+        String transferString = "hello_from_client";
         Scanner keyboard = new Scanner(System.in);
         System.out.println("Ping server? Y/N: ");
         String answer = keyboard.next().toLowerCase();
         char proceed = answer.charAt(0);
-        do{
+        while(proceed == 'y'){
+            transferData = transferString.getBytes();
 
-        }while()
+            // create packet to send
+            DatagramPacket sendPacket = new DatagramPacket(transferData, transferData.length, IPAddress, port);
+
+            for(int attempts = 0; attempts <10; attempts ++){
+                long start = System.currentTimeMillis();
+
+                uplink.send(sendPacket);
+
+                DatagramPacket receivePacket = new DatagramPacket(receiveData, receiveData.length);
+
+                uplink.setSoTimeout(1000);
+
+                try {
+                    uplink.receive(receivePacket);
+
+                    long finish = System.currentTimeMillis();
+                    long rtt = finish - start;
+
+                    System.out.println("Round trip time (RTT): " + rtt);
+
+                    String incoming = new String(receivePacket.getData());
+                    System.out.println("From server: " + incoming);
+
+                } catch (SocketTimeoutException ex) {
+                    System.out.println("Request timed out.");
+                }
+            }
+
+            System.out.println("Ping server again? Y/N: ");
+            answer = keyboard.next().toLowerCase();
+            proceed = answer.charAt(0);
+        }
+        uplink.close();
+
 
     }
 }
